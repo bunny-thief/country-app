fff7abe06ccbeb4b9b2401a4c6e513c920826e18
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2023-02-23 22:23:39 +0100

    Add Mongodb, dotenv

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
-const PORT = 5000
+const MongoClient = require('mongodb').MongoClient
+
+require('dotenv').config()
+const connection_string = process.env.CONNECTION_STRING
+const database = process.env.DB
+const collection = process.env.COLLECTION
+const PORT = process.env.PORT || 5000
+
+MongoClient.connect(connection_string, { useUnifiedTopology: true })
+    .then(client => {
+        const db = client.db(database)
+        const countries = db.collection(collection)
+        console.log(`Connected to database`)
+    })
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
