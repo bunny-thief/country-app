60983bab3f2ab2caf70687b7a5abecadcdb21b2d
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2023-02-25 08:27:01 +0100

    Add '/api/countries/capital/:capital' route

--- app.js
+++ app.js
@@ -27,9 +27,15 @@ app.get('/api/countries/:country', (async (req, res) => {
     res.json(country)
 }))
 
+app.get('/api/countries/capital/:capital', async (req, res) => {
+    const { country } = await db.collection(collection).findOne({ capital: req.params.capital })
+    res.json(country)
+})
+
 app.get('/api/capitals/:country', (async (req, res) => {
     const country = await db.collection(collection).findOne({ country: req.params.country })
     res.json(country.capital)
 }))
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
